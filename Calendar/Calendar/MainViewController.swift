45ab307f88a6ab871aa9cffac2a5a057ca8a2d47
H0sungKim <hyongak516@mail.hongik.ac.kr>
2024-04-08 13:34:35 +0900

    deal date data

--- Calendar/Calendar/MainViewController.swift
+++ Calendar/Calendar/MainViewController.swift
@@ -10,6 +10,8 @@ import EventKit
 
 class MainViewController: UIViewController {
     
+    // while 그 전 달 토요일까지
+    
     @IBOutlet weak var lbMonth: UILabel!
     @IBOutlet weak var lbDay: UILabel!
     
@@ -34,9 +36,27 @@ class MainViewController: UIViewController {
         year = calendar.component(.year, from: now)
         month = calendar.component(.month, from: now)
         day = calendar.component(.day, from: now)
+        
+        var dateComponents = DateComponents()
+        dateComponents.year = 2023
+        dateComponents.month = 11
+        dateComponents.day = 15
+        let sample = calendar.date(from: dateComponents)
+        let sample2 = calendar.date(byAdding: .day, value: 1, to: sample!)
+        
+        NSLog("\(calendar.component(.weekOfMonth, from: sample!))")
+        NSLog("\(calendar.component(.weekday, from: sample!))")
+        
+        NSLog("\(calendar.component(.weekOfMonth, from: sample2!))")
+        NSLog("\(calendar.component(.weekday, from: sample2!))")
+        
+        NSLog("\(calendar.component(.weekOfMonth, from: now))")
+        NSLog("\(calendar.component(.weekday, from: now))")
+        
         NSLog("\(year) \(month) \(day)")
         lbMonth.text = "\(year)년 \(month)월"
         lbDay.text = "\(day)일"
+//        Date
     }
     
     func addEventToCalendar() {
